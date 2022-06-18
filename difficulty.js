3b21d2909fc814182c8490d2d03038e35e2fee7c
Hazem Ammar <45862155+Hazem-A@users.noreply.github.com>
2022-06-18 15:27:06 -0600

    Update difficulty.js

--- difficulty.js
+++ difficulty.js
@@ -1,6 +1,6 @@
 
 // add the difficulty back into kattis
-
+import {prob_map} from "./import_difficulty";
 let container = document.getElementsByClassName("attribute_list-book")[0]
 
 let diff = document.createElement("div")
@@ -9,4 +9,14 @@ let s = document.createElement("span")
 s.className="attribute_list-label"
 s.innerHTML="Difficulty"
 diff.appendChild(s)
+
+let prob_name = document.getElementsByClassName("book-page-heading")
+if (prob_map.has(prob_name)){
+  let s2 = document.createElement("span")
+// check to see if the problem name is in the map
+let diff_num = prob_map.get(prob_name);
+}
+
+
+
 container.appendChild(diff)
