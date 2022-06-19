407a9e2fd7574d34a1b7ea7b44fd0dee3e1ed83d
Hazem Ammar <45862155+Hazem-A@users.noreply.github.com>
2022-06-19 13:57:41 -0600

    Add files via upload

--- import_difficulty.js
+++ import_difficulty.js
@@ -2,6 +2,27 @@
 
 // run this on any kattis problems page
 
+chrome.tabs.onUpdated.addListener((tabId,tab) => {
+    if (tab.url && tab.url=== "https://open.kattis.com/problems"){
+        let names = document.querySelectorAll(".table2 tbody tr td:nth-child(1) a")
+        let diff_number = document.querySelectorAll(".difficulty_number")
+
+        let prob_map = new Map()
+        let n = [];
+        for (let i of names){
+            n.push(i.innerHTML)
+        }
+        let p = []
+        for (let i of diff_number){
+            p.push(i.innerHTML)
+        }
+
+        for (let i = 0; i < n.length;i++){
+            prob_map.set(n[i],p[i]);
+        }
+        chrome.tabs.sendMessage(tabId,{map:prob_map})
+    }
+})
 let names = document.querySelectorAll(".table2 tbody tr td:nth-child(1) a")
 let diff_number = document.querySelectorAll(".difficulty_number")
 
