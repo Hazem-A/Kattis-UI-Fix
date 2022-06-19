341efa16662951ea1954379049653130b8407abe
Hazem Ammar <45862155+Hazem-A@users.noreply.github.com>
2022-06-19 14:26:01 -0600

    Update import_difficulty.js

--- import_difficulty.js
+++ import_difficulty.js
@@ -23,20 +23,5 @@ chrome.tabs.onUpdated.addListener((tabId,tab) => {
         chrome.tabs.sendMessage(tabId,{map:prob_map})
     }
 })
-let names = document.querySelectorAll(".table2 tbody tr td:nth-child(1) a")
-let diff_number = document.querySelectorAll(".difficulty_number")
 
-export let prob_map = new Map()
-let n = [];
-for (let i of names){
-    n.push(i.innerHTML)
-}
-let p = []
-for (let i of diff_number){
-    p.push(i.innerHTML)
-}
-
-for (let i = 0; i < n.length;i++){
-    prob_map.set(n[i],p[i]);
-}
 
